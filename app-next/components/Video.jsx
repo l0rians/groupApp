c5627cc878420fee5803e57a5dbb97cd4f4a550f
melo4yxa77796 <g_julija_777@mail.ru>
2025-02-02 17:32:34 +0100

    sync rooms4

--- app-next/components/Video.jsx
+++ app-next/components/Video.jsx
@@ -6,33 +6,35 @@ import { getRoom, updateRoom } from "@/roomActions";
 
 export function Video ({ id, url, cursor, state }) {
   const playedSeconds = useRef(cursor);
-  const [videoState, setVideoState] = useState(state);
+  const videoState = useRef(state);
 
   async function refreshRoom(){
     const room = await getRoom(id);
     playedSeconds.current = room.cursor;
+    videoState.current = room.state;
   }
 
   useEffect(() => {
-    refreshRoom();
+    const intervalId = setInterval(refreshRoom, 3000);
+    return () => clearInterval(intervalId);
   }, []);
 
   function saveRoom() {
-    updateRoom({ id, cursor: playedSeconds.current, video_state: videoState });
+    updateRoom({ id, cursor: playedSeconds.current, video_state: videoState.current });
   }
 
   return <ReactPlayer url={url} width={"100%"} height={"100%"} onProgress={x => {
     playedSeconds.current = parseInt(x.playedSeconds);
     saveRoom();
   }} controls
-  playing={videoState === 'playing'}
-  muted={videoState === 'playing'}
+  playing={videoState.current === 'playing'}
+  muted={videoState.current === 'playing'}
   onPlay={() => {
-    setVideoState('playing');
+    videoState.current = 'playing';
     saveRoom();
   }}
   onPause={() => {
-    setVideoState('paused');
+    videoState.current = 'paused';
     saveRoom();
   }}
   config={{
